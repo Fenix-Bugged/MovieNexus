c4e41b0685d27dfbfa9ff1afaaff46dbf436e920
Fenix-Bugged <guacanemeoscarfelipe@gmail.com>
2026-05-21 17:19:31 -0500

    feat: implementar View Transitions API para navegacion fluida estilo app nativa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,19 @@ import { apiInterceptor } from './core/interceptors/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions(), withComponentInputBinding()), // Activamos las transiciones de vista aquí
+    provideRouter(
+      routes,
+      withViewTransitions({
+        onViewTransitionCreated: ({ transition, from }) => {
+          // Si no hay una ruta previa (from es undefined), estamos en la carga inicial de la aplicación
+          // por lo que cancelamos la transición para que se cargue al instante sin animación lenta.
+          if (!from) {
+            transition.skipTransition();
+          }
+        }
+      }),
+      withComponentInputBinding()
+    ), // Activamos las transiciones de vista aquí
     provideClientHydration(),
     provideHttpClient(
       withFetch(),
