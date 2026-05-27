49399a7f5d08fd72c8cbd3f667f6c6c6e72cf0e2
Fenix-Bugged <guacanemeoscarfelipe@gmail.com>
2026-05-27 14:56:30 -0500

    fix: agregar fallback de api_key por defecto para evitar error de carga en Vercel

--- set-env.js
+++ set-env.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 
-// process.env.API_KEY leerá la variable de entorno que configures en la web de Vercel
+// Si no está configurada la variable en Vercel, usamos la clave por defecto para que funcione out-of-the-box
+const apiKey = process.env.API_KEY || '5a0d01d8dd4118e73182c8828d58f5c7';
+
 const envConfigFile = `export const environment = {
   production: true,
   baseUrl: 'https://api.themoviedb.org/3',
-  apiKey: '${process.env.API_KEY}',
+  apiKey: '${apiKey}',
   imgPath: 'https://image.tmdb.org/t/p'
 };
 `;
