84e5f308c38cf917f629d8f98bc1d5aa4d2ba9e5
Fenix-Bugged <guacanemeoscarfelipe@gmail.com>
2026-05-27 14:51:04 -0500

    fix: resolver importaciones obsoletas de environment.development y eliminar fileReplacements

--- src/app/core/interceptors/api-key.interceptor.ts
+++ src/app/core/interceptors/api-key.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 /**
  * Functional interceptor that appends the TMDB api_key and language
